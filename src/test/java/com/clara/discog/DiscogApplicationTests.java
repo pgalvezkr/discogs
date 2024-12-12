1b6f99037d6648de629168c4ea0187affbc42561
pgalvezkr <patti_gm2010@live.com>
2024-12-12 10:33:50 -0500

    First version of client
    First version of client for consume API and get artists

---
+++ src/test/java/com/clara/discog/DiscogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clara.discog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
